57a396744d1dc7fa9163bcd8db0b672d36ce2bc8
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-16 18:40:13 +0530

    created ChartCard component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 import { ThemeContext } from './AllContexts';
 import Button from './components/Button/Button';
 import SuggestCard from './components/SuggestCard/SuggestCard';
+import ChatCard from './components/ChatCard/ChatCard';
+
+
+import icon1 from "./assets/icon2.png";
 
 function App() {
   const [theme, setTheme] = useState("light")
@@ -14,6 +18,7 @@ function App() {
       <main>
         <Button text="save"/>
         <SuggestCard mainText="Hi, what is my location" subText="Get immediate AI generated response" />
+        <ChatCard name="you" message="Hi There. How can I assist you today?" time="10:34 AM" icon={icon1}/>
       </main>
     </ThemeContext.Provider>
     </>
---
+++ src/components/ChatCard/ChatCard.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+//styles
+import "./ChatCard.css";
+//contexts
+import { ThemeContext } from '../../AllContexts';
+
+const ChatCard = props => {
+    //props
+    const { name, message, time, icon } = props;
+    // ..contexts
+    const [theme, setTheme] = useContext(ThemeContext);
+    return (
+        <div className={`ChatCard ChatCardTheme-${theme}`}>
+            <span className='chatCardImage-wrapper'>
+                <img src={icon} alt={`${name} icon`} className='chatCardImage'/>
+            </span>
+            <span className='chatCardTexts'>
+                <span className='chatCardName'>{name}</span>
+                <span className='chatCardMessage'>{message}</span>
+                <span className='chatCardTime'>{time}</span>
+            </span>
+        </div>
+    );
+};
+
+export default ChatCard;
