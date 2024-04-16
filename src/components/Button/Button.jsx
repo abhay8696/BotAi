e299412a535e1ff7754736b40847e88a60f78412
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-16 16:09:40 +0530

    created Button component and gave responsive styles

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+//styles
+import "./Button.css";
+//contexts
+import { ThemeContext } from '../../AllContexts';
+
+const Button = props => {
+    //props
+    const { clickFunction, text } = props;
+    //context
+    const [theme, setTheme] = useContext(ThemeContext)
+    return (
+        <button className={`Button ButtonTheme${theme}`}>
+            {text || "text"}
+        </button>
+    );
+};
+
+export default Button;
